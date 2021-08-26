bb401c9fd260e5ed10d1ff33302b8f1510327f6d
Akrylatt <akrylatt@gmail.com>
2021-08-26 16:39:42 +0200

    add solution for the second lab

--- week2/ex3.c
+++ week2/ex3.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-  int hight;
+  int hight = 1;
+  if(argc > 1){
+    sscanf(argv[1], "%d", &hight);
+  }
+  
 
-  scanf(("%d"), &hight);
+  // scanf(("%d"), &hight);
 
   for( int i = 0; i <= hight; i++){
     for(int k = 0; k <= (((2*hight) - 1) - ((2 * i) - 1))/ 2; k++){
